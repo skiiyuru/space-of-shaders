30c2cb0329f4057843796dd1575560e897b6d0e1
StephenKiiyuruNganga <steveknganga@gmail.com>
2025-02-27 14:30:26 +0300

    add canvas click feature

--- src/components/fireworks/Fireworks.tsx
+++ src/components/fireworks/Fireworks.tsx
@@ -1,13 +1,25 @@
-import { useEffect, useRef } from 'react'
 import { useLoader, useThree } from '@react-three/fiber'
-import * as THREE from 'three'
-import vertex_shader from '../../shaders/firework/vertex.glsl?raw'
-import fragment_shader from '../../shaders/firework/fragment.glsl?raw'
 import gsap from 'gsap'
+import type { ForwardedRef } from 'react'
+import { useImperativeHandle } from 'react'
+import * as THREE from 'three'
+import fragment_shader from '../../shaders/firework/fragment.glsl?raw'
+import vertex_shader from '../../shaders/firework/vertex.glsl?raw'
 
-export default function Fireworks() {
-  const ref = useRef<THREE.Points>(null!)
+export type CreateFireWork = (
+  count: number,
+  position: THREE.Vector3,
+  size: number,
+  texture: THREE.Texture,
+  radius: number,
+  color: THREE.Color
+) => void
 
+type FireWorks = {
+  ref: ForwardedRef<unknown>
+}
+
+export default function Fireworks({ ref }: FireWorks) {
   // Get the size of the canvas so that we can resize the points
   const {
     scene,
@@ -26,96 +38,95 @@ export default function Fireworks() {
     '/textures/particles/8.png',
   ])
 
-  function createFirework(
-    firework: THREE.Points,
-    count: number,
-    position: THREE.Vector3,
-    size: number,
-    texture: THREE.Texture,
-    radius: number,
-    color: THREE.Color
-  ) {
-    const vertex_positions = new Float32Array(count * 3)
-    const vertex_sizes = new Float32Array(count)
+  useImperativeHandle(
+    ref,
+    () => {
+      return {
+        textures,
+        createFirework: function (
+          count,
+          position,
+          size,
+          texture,
+          radius,
+          color
+        ) {
+          const vertex_positions = new Float32Array(count * 3)
+          const vertex_sizes = new Float32Array(count)
 
-    for (let i = 0; i < count; i++) {
-      // Index for every 3rd position in the array
-      const i3 = i * 3
+          for (let i = 0; i < count; i++) {
+            // Index for every 3rd position in the array
+            const i3 = i * 3
 
-      // Create a position that references a sphere shape
-      const spherical_position = new THREE.Spherical(
-        radius * (0.75 + Math.random() * 0.25),
-        Math.random() * Math.PI,
-        Math.random() * Math.PI * 2
-      )
+            // Create a position that references a sphere shape
+            const spherical_position = new THREE.Spherical(
+              radius * (0.75 + Math.random() * 0.25),
+              Math.random() * Math.PI,
+              Math.random() * Math.PI * 2
+            )
 
-      const position = new THREE.Vector3()
-      position.setFromSpherical(spherical_position)
+            const position = new THREE.Vector3()
+            position.setFromSpherical(spherical_position)
 
-      vertex_positions[i3] = position.x
-      vertex_positions[i3 + 1] = position.y
-      vertex_positions[i3 + 2] = position.z
+            vertex_positions[i3] = position.x
+            vertex_positions[i3 + 1] = position.y
+            vertex_positions[i3 + 2] = position.z
 
-      vertex_sizes[i] = Math.random()
-    }
+            vertex_sizes[i] = Math.random()
+          }
 
-    const geometry = new THREE.BufferGeometry()
-    geometry.setAttribute(
-      'position',
-      new THREE.Float32BufferAttribute(vertex_positions, 3)
-    )
-    geometry.setAttribute('a_size', new THREE.BufferAttribute(vertex_sizes, 1))
+          const geometry = new THREE.BufferGeometry()
+          geometry.setAttribute(
+            'position',
+            new THREE.Float32BufferAttribute(vertex_positions, 3)
+          )
+          geometry.setAttribute(
+            'a_size',
+            new THREE.BufferAttribute(vertex_sizes, 1)
+          )
 
-    // ThreeJS by default flips textures upside down, disable this
-    texture.flipY = false
+          // ThreeJS by default flips textures upside down, disable this
+          texture.flipY = false
 
-    const material = new THREE.ShaderMaterial({
-      vertexShader: vertex_shader,
-      fragmentShader: fragment_shader,
-      uniforms: {
-        u_size: new THREE.Uniform(size),
-        u_resolution: new THREE.Uniform(new THREE.Vector2(width, height)),
-        u_texture: new THREE.Uniform(texture),
-        u_color: new THREE.Uniform(color),
-        u_progress: new THREE.Uniform(0),
-      },
-      transparent: true,
-      depthWrite: false,
-      blending: THREE.AdditiveBlending,
-    })
+          const material = new THREE.ShaderMaterial({
+            vertexShader: vertex_shader,
+            fragmentShader: fragment_shader,
+            uniforms: {
+              u_size: new THREE.Uniform(size),
+              u_resolution: new THREE.Uniform(new THREE.Vector2(width, height)),
+              u_texture: new THREE.Uniform(texture),
+              u_color: new THREE.Uniform(color),
+              u_progress: new THREE.Uniform(0),
+            },
+            transparent: true,
+            depthWrite: false,
+            blending: THREE.AdditiveBlending,
+          })
 
-    // Add the geometry, material & position to the firework object
-    firework.geometry = geometry
-    firework.material = material
-    firework.position.copy(position)
+          // Create firework object
+          const firework = new THREE.Points(geometry, material)
+          firework.position.copy(position)
+          scene.add(firework)
 
-    // Destroy firework helper
-    function destroy() {
-      scene.remove(firework)
-      geometry.dispose()
-      material.dispose()
-    }
+          // Destroy firework helper
+          function destroy() {
+            scene.remove(firework)
+            geometry.dispose()
+            material.dispose()
+          }
 
-    // Animate
-    gsap.to(material.uniforms.u_progress, {
-      value: 1,
-      duration: 3,
-      ease: 'linear',
-      onComplete: destroy,
-    })
-  }
+          // Animate
+          gsap.to(material.uniforms.u_progress, {
+            value: 1,
+            duration: 3,
+            ease: 'linear',
+            onComplete: destroy,
+          })
+        } as CreateFireWork,
+      }
+    },
+    []
+  )
 
-  useEffect(() => {
-    createFirework(
-      ref.current,
-      100,
-      new THREE.Vector3(),
-      0.5,
-      textures[7],
-      1,
-      new THREE.Color('#0AE448')
-    )
-  }, [])
-
-  return <points ref={ref} />
+  return null
 }
--- src/components/fireworks/index.tsx
+++ src/components/fireworks/index.tsx
@@ -1,11 +1,33 @@
-import { OrbitControls, Stage } from '@react-three/drei'
+import { OrbitControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import { Perf } from 'r3f-perf'
-import Fireworks from './Fireworks'
+import { useRef } from 'react'
+import * as THREE from 'three'
+import Fireworks, { type CreateFireWork } from './Fireworks'
 
 export default function Experience() {
+  const firework_ref = useRef<{
+    textures: THREE.Texture[]
+    createFirework: CreateFireWork
+  }>(null!)
+
+  function handleClick() {
+    firework_ref.current.createFirework(
+      100,
+      new THREE.Vector3(),
+      0.5,
+      firework_ref.current.textures[7],
+      1,
+      new THREE.Color('#0AE448')
+    )
+  }
+
   return (
-    <Canvas camera={{ position: [0, 0, 5], fov: 35 }} dpr={[1, 2]}>
+    <Canvas
+      camera={{ position: [0, 0, 5], fov: 35 }}
+      dpr={[1, 2]}
+      onClick={handleClick}
+    >
       <color attach="background" args={['#0e100f']} />
 
       <Perf position="bottom-right" />
@@ -16,7 +38,7 @@ export default function Experience() {
         makeDefault
       />
 
-      <Fireworks />
+      <Fireworks ref={firework_ref} />
     </Canvas>
   )
 }
